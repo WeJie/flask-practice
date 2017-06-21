63c11ccad06ec2cedd5fc79a661f5fc72e86d420
weijie <wejie00@foxmail.com>
2017-06-21 23:10:15 +0800

    Add user profile editor page

--- app/main/forms.py
+++ app/main/forms.py
@@ -9,3 +9,11 @@ from wtforms import ValidationError
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
