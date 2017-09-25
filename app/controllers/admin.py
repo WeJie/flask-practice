dd87791f1470e17fcfc60666fa0796361d4da44d
weijie <wejie00@foxmail.com>
2017-09-25 20:23:39 +0800

    Add log require

--- app/controllers/admin.py
+++ app/controllers/admin.py
@@ -1,15 +1,18 @@
 # -*- coding:utf-8 -*-
 from flask_admin import BaseView, expose
 from flask_admin.contrib.sqla import ModelView
+from flask_login import login_required
 
 
 class CustomView(BaseView):
     
     @expose('/')
+    @login_required
     def index(self):
         return self.render('/admin/custom.html')
 
     @expose('/second_page')
+    @login_required
     def second_page(self):
         return self.render('admin/second_page.html')
 
